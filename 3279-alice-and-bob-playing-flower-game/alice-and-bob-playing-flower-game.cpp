df1c43a8c2b258a0fbb8ce3a116ae81287025a1c
Soriful Islam Sk <sorifulislamsk1@gmail.com>
2025-08-29 11:19:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (86.22%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
@@ -1,6 +1,33 @@
+// class Solution {
+// public:
+//     long long flowerGame(int n, int m) {
+//         return 1LL * n * m / 2;
+//     }
+// };
+
+
+
 class Solution {
 public:
     long long flowerGame(int n, int m) {
-        return 1LL * n * m / 2;
+        long e1,o1,e2,o2=0;
+        long ans=0;
+
+        if(n%2==0){
+            e1=n/2,o1=n/2;
+        }
+        else{
+            e1=n/2,o1=(n/2 +1);
+        }
+
+        if(m%2==0){
+            e2=m/2,o2=m/2;
+        }
+        else{
+            e2=m/2,o2=(m/2 +1);
+        }
+
+        ans+=1L*e1*o2+e2*o1;
+        return ans;
     }
 };
