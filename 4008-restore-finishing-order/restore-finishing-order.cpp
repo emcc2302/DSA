3b9975458072d63afc6dbbab108a143b9b6ccb61
Soriful Islam Sk <sorifulislamsk1@gmail.com>
2025-09-02 23:51:35 +0530

    Time: 0 ms (100.00%) | Memory: 50.2 MB (73.18%) - LeetSync

--- 4008-restore-finishing-order/restore-finishing-order.cpp
+++ 4008-restore-finishing-order/restore-finishing-order.cpp
@@ -1,14 +1,35 @@
+// #include <vector>
+// #include <unordered_set>
+// using namespace std;
+
+// class Solution {
+// public:
+//     vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
+//         unordered_set<int> wanted(friends.begin(), friends.end());
+//         vector<int> res;
+//         for (int x : order)
+//             if (wanted.count(x)) res.push_back(x);
+//         return res;
+//     }
+// };
+
+
 #include <vector>
-#include <unordered_set>
 using namespace std;
 
 class Solution {
 public:
     vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
-        unordered_set<int> wanted(friends.begin(), friends.end());
         vector<int> res;
-        for (int x : order)
-            if (wanted.count(x)) res.push_back(x);
+        for (int x : order) {
+           
+            for (int y : friends) {
+                if (x == y) {
+                    res.push_back(x);
+                    break; 
+                }
+            }
+        }
         return res;
     }
 };
