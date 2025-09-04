f64c402cf7932c3626a8b61d25e7a9cf83b1a8e6
Soriful Islam Sk <sorifulislamsk1@gmail.com>
2025-09-04 23:57:09 +0530

    Time: 1 ms (6.52%) | Memory: 8.4 MB (88.80%) - LeetSync

--- 3830-find-closest-person/find-closest-person.cpp
+++ 3830-find-closest-person/find-closest-person.cpp
@@ -1,8 +1,18 @@
+// class Solution {
+// public:
+//     int findClosest(int x, int y, int z) {
+//         if(abs((z-x))<abs((z-y))) return 1;
+//         else if(abs((z-x))>abs((z-y))) return 2;
+//         else return 0;
+//     }
+// };
+
+
+//using ternary operator 
+
 class Solution {
 public:
     int findClosest(int x, int y, int z) {
-        if(abs((z-x))<abs((z-y))) return 1;
-        else if(abs((z-x))>abs((z-y))) return 2;
-        else return 0;
+        return abs(z - x) < abs(z - y) ? 1 : (abs(z - x) > abs(z - y) ? 2 : 0);
     }
 };
