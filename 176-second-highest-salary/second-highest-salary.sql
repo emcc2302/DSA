26502b0c7045cc423aaa9764961d9ac080d03453
Soriful Islam Sk <sorifulislamsk1@gmail.com>
2025-10-15 22:13:39 +0530

    Time: 271 ms (47.27%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -5,3 +5,8 @@ SELECT
        ORDER BY Salary DESC
         LIMIT 1 OFFSET 1),
     NULL) AS SecondHighestSalary
+
+    /* IFNULL(expression, value_if_null)
+    If expression is NOT NULL, return expression.
+
+      If expression IS NULL, return value_if_null.*/
