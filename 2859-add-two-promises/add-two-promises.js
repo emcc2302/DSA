61ee3ee8a7544bf59ea648b842d92e33d5a0d12d
Soriful Islam Sk <sorifulislamsk1@gmail.com>
2026-03-06 23:48:27 +0530

    Time: 64 ms (9.66%) | Memory: 53.9 MB (84.06%) - LeetSync

--- 2859-add-two-promises/add-two-promises.js
+++ 2859-add-two-promises/add-two-promises.js
@@ -5,6 +5,9 @@
  */
 var addTwoPromises = async function(promise1, promise2) {
     const [value1,value2]=await Promise.all([promise1, promise2]);
+    //We use Promise.all to run multiple promises concurrently and wait for all of them.
+
+    //and we use [] bcz promise take only one iterate ,so for 2 iterate we must need an array 
     return (value1+value2);
 };
 
