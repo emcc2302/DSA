2584ef23c0b66ef5d8744a1f7895e349c4ee7a37
Soriful Islam Sk <sorifulislamsk1@gmail.com>
2026-04-10 22:53:35 +0530

    Time: 83 ms (50.85%) | Memory: 88.9 MB (65.82%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -5,7 +5,7 @@ public:
         unordered_set<int>s(nums.begin(),nums.end());
 
         for(int num :s){
-            if(s.find(num-1)==s.end()){
+            if(s.find(num-1)==s.end()){//num will be the first or current  number if num-1 isn't exist
                 int current = num;
                 int count = 1;
             
